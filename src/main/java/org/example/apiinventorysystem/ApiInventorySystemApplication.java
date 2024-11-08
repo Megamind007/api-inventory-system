8633e48e8d3532644cdd9437aa93ad21dcbdea76
Megamind <soy.tola9999@gmail.com>
2024-11-08 10:58:40 +0700

    first commit

---
+++ src/main/java/org/example/apiinventorysystem/ApiInventorySystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.apiinventorysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInventorySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/apiinventorysystem/ApiInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apiinventorysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
